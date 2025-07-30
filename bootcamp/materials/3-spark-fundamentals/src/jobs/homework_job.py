a569d45da689a20c036dca18b5e20aee9d2988c2
Alvaro Vilaplana <alvarovilaplana@Mac.fritz.box>
2025-07-30 12:55:18 +0100

    3 spark fundamentals homework WIP

--- bootcamp/materials/3-spark-fundamentals/src/jobs/homework_job.py
+++ bootcamp/materials/3-spark-fundamentals/src/jobs/homework_job.py
@@ -1,6 +1,20 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, broadcast
 
+# Spark Fundamentals Week
+
+# - match_details
+#   - a row for every players performance in a match
+# - matches
+#   - a row for every match
+# - medals_matches_players
+#   - a row for every medal type a player gets in a match
+# - medals
+#   - a row for every medal type
+#
+#
+# Your goal is to make the following things happen:
+
 spark = SparkSession.builder \
             .master("local") \
             .appName("homework") \
@@ -35,6 +49,8 @@ match_details_df = spark \
                     .option("header", "true") \
                     .csv("../../data/match_details.csv")
 
+# print("match_details_df")
+# match_details_df.orderBy(col("match_id")).show(20)
 
 # Extract matches data set
 matches_df = spark \
@@ -42,22 +58,58 @@ matches_df = spark \
                 .option("header","true") \
                 .csv("../../data/matches.csv")
 
+# print("matches_df")
+# matches_df.orderBy(col("match_id")).show(20)
+
 # Extract medal_matches_players data set
 medal_matches_players_df = spark \
                             .read \
                             .option("header","true") \
-                            .csv("../../data/medals_matches_players.csv")
+                            .csv("../../data/medals_matches_players.csv") \
+                            .withColumnRenamed("player_gamertag","player_gamertag_medal")
 
-matches_join_df = matches_df \
-                    .join(match_details_df, on="match_id", how="inner") \
-                    .join(medal_matches_players_df, on="match_id", how="inner")
+# print("medal_matches_players_df")
+# medal_matches_players_df.orderBy(col("match_id")).show(20)
 
-sorted_matches_join_df = matches_join_df \
-                            .repartition(16, col("match_id"))
+# Alias your DataFrames
+matches_alias_df = matches_df.alias("m")
+match_details_alias_df = match_details_df.alias("md")
+medal_matches_players_alias_df = medal_matches_players_df.alias("mmp")
+
+matches_join_df = matches_alias_df \
+                    .join(match_details_alias_df, on="match_id", how="inner") \
+                    .join(medal_matches_players_alias_df, on="match_id", how="inner")
+
+
+repartition_matches_join_df = matches_join_df \
+                            .repartition(16, col("m.match_id"))
+
+# repartition_matches_join_df.printSchema()
 
 # - Aggregate the joined data frame to figure out questions like:
 #     - Which player averages the most kills per game?
+            # note: interesting fields: player_gamertag, player_total_kills
 
+repartition_matches_join_df.createOrReplaceTempView("repartition_matches_join_df")
+
+first_question_query = """
+WITH players_match_with_row_number (
+    SELECT 
+        *,
+        ROW_NUMBER() OVER (PARTITION BY match_id, player_gamertag ORDER BY match_id) AS row_num    
+    FROM repartition_matches_join_df        
+), players_match_deduped AS (
+    SELECT *
+    FROM players_match_with_row_number
+    WHERE row_num = 1                
+    )
+    
+    SELECT * FROM players_match_deduped
+"""
+
+q_df = spark.sql(first_question_query)
+
+q_df.show(15)
 
 #     - Which playlist gets played the most?
 #     - Which map gets played the most?
