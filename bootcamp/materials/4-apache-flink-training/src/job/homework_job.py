ac00c68c6b2f6e7e7fdc78abe5df1f760a76a790
Alvaro Vilaplana <alvarovilaplana@Alvaros-MacBook-Pro.local>
2025-08-12 13:21:53 +0200

    Homework 4-apache-flink-training

---
+++ bootcamp/materials/4-apache-flink-training/src/job/homework_job.py
@@ -0,0 +1,117 @@
+import os
+from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.table import EnvironmentSettings, DataTypes, TableEnvironment, StreamTableEnvironment
+from pyflink.table.expressions import lit, col
+from pyflink.table.window import Session
+
+# Homework
+
+# - Create a Flink job that sessionizes the input data by IP address and host
+# - Use a 5 minute gap
+
+def create_aggregated_events_sink_postgres(t_env):
+    table_name = 'processed_events_aggregated'
+    sink_ddl = f"""
+        CREATE TABLE {table_name} (
+            event_hour TIMESTAMP(3),
+            host VARCHAR,
+            ip VARCHAR,
+            num_hits BIGINT
+        ) WITH (
+            'connector' = 'jdbc',
+            'url' = '{os.environ.get("POSTGRES_URL")}',
+            'table-name' = '{table_name}',
+            'username' = '{os.environ.get("POSTGRES_USER", "postgres")}',
+            'password' = '{os.environ.get("POSTGRES_PASSWORD", "postgres")}',
+            'driver' = 'org.postgresql.Driver'
+        );
+    """
+    t_env.execute_sql(sink_ddl)
+    return table_name
+
+def create_processed_events_source_kafka(t_env):
+    kafka_key = os.environ.get("KAFKA_WEB_TRAFFIC_KEY", "")
+    kafka_secret = os.environ.get("KAFKA_WEB_TRAFFIC_SECRET", "")
+    table_name = "process_events_kafka"
+    pattern = "yyyy-MM-dd''T''HH:mm:ss.SSS''Z''"
+    sink_ddl = f"""
+        CREATE TABLE {table_name} (
+            ip VARCHAR,
+            event_time VARCHAR,
+            referrer VARCHAR,
+            host VARCHAR,
+            url VARCHAR,
+            geodata VARCHAR,
+            window_timestamp AS TO_TIMESTAMP(event_time, '{pattern}'),
+            WATERMARK FOR window_timestamp AS window_timestamp - INTERVAL '15' SECOND
+        ) WITH (
+             'connector' = 'kafka',
+            'properties.bootstrap.servers' = '{os.environ.get('KAFKA_URL')}',
+            'topic' = '{os.environ.get('KAFKA_TOPIC')}',
+            'properties.group.id' = '{os.environ.get('KAFKA_GROUP')}',
+            'properties.security.protocol' = 'SASL_SSL',
+            'properties.sasl.mechanism' = 'PLAIN',
+            'properties.sasl.jaas.config' = 'org.apache.flink.kafka.shaded.org.apache.kafka.common.security.plain.PlainLoginModule required username=\"{kafka_key}\" password=\"{kafka_secret}\";',
+            'scan.startup.mode' = 'latest-offset',
+            'properties.auto.offset.reset' = 'latest',
+            'format' = 'json'
+        );
+    """
+    t_env.execute_sql(sink_ddl)
+    return table_name
+
+
+def log_aggregation():
+    # Set up the execution environment
+    env = StreamExecutionEnvironment.get_execution_environment()
+    env.enable_checkpointing(10)
+    env.set_parallelism(3)
+
+    # Set up the table environment
+    settings = EnvironmentSettings.new_instance().in_streaming_mode().build()
+    t_env = StreamTableEnvironment.create(env, environment_settings=settings)
+
+    try:
+        # Create Kafka table
+        source_table = create_processed_events_source_kafka(t_env)
+
+        aggregated_table = create_aggregated_events_sink_postgres(t_env)
+        t_env.from_path(source_table)\
+            .window(
+            Session.with_gap(lit(5).minutes).on(col("window_timestamp")).alias("w")
+        ).group_by(
+            col("w"),
+            col("host"),
+            col("ip")
+        ) \
+            .select(
+                    col("w").start.alias("event_hour"),
+                    col("host"),
+                    col("ip"),
+                    col("host").count.alias("num_hits")
+            ) \
+            .execute_insert(aggregated_table) \
+            .wait()
+
+    except Exception as e:
+        print("Writing records from Kafka to JDBC failed:", str(e))
+
+
+if __name__ == '__main__':
+    log_aggregation()
+
+
+# - Answer these questions
+#   - What is the average number of web events of a session from a user on Tech Creator?
+#     SELECT
+#         AVG(num_hits) AS avg_hits_per_session
+#     FROM processed_events_aggregated
+#     WHERE host LIKE '%techcreator.io';
+
+#   - Compare results between different hosts (zachwilson.techcreator.io, zachwilson.tech, lulu.techcreator.io)
+#     SELECT
+#         host,
+#         AVG(num_hits) AS avg_hits_per_session
+#     FROM processed_events_aggregated
+#     GROUP BY host
+#     HAVING BY host IN ('zachwilson.techcreator.io', 'zachwilson.tech', 'lulu.techcreator.io')
